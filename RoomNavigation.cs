4f8c4673a1ca7c6550c79ee8448ff4e22f399a5d
jal220 <31681394+jal220@users.noreply.github.com>
2017-10-16 15:45:35 -0500

    Add files via upload

---
+++ RoomNavigation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour
+{
+    public Room currentRoom;
+
+    Dictionary<string, Room> exitDictionary = new Dictionary<string, Room>();
+
+    GameController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
+
+    public void UnpackExitsInRoom()
+    {
+        for (int i = 0; i < currentRoom.exits.Length; i++)
+        {
+            exitDictionary.Add(currentRoom.exits[i].keyString, currentRoom.exits[i].valueRoom);         
+            controller.interactionDescriptionsInRoom.Add(currentRoom.exits[i].exitDescription);
+        }
+    }
+
+    public void AttemptToChangeRooms(string directionNoun)
+    {
+        if (exitDictionary.ContainsKey(directionNoun))
+        {
+            currentRoom = exitDictionary[directionNoun];
+            controller.LogStringWithReturn("You go to play " + directionNoun);
+            controller.DisplayRoomText();
+        }
+        else
+        {
+            controller.LogStringWithReturn("You head over to the arcade cabinet titled " + directionNoun + ". You're surprised to find that it actually has a video game version of " + directionNoun + ". Which is as terrible as it sounds.");
+        }
+    }
+
+    public void ClearExits()
+    {
+        exitDictionary.Clear();
+    }
+}
