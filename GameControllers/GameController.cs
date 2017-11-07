f4312985a35a7e4e2524418386d0fa93d6c1d1c3
jmass1991 <31499031+jmass1991@users.noreply.github.com>
2017-11-07 17:48:46 -0600

    Game Controllers

---
+++ GameControllers/GameController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+
+    private int waveCount;
+    private int currentScore;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+    public GUIText levelText;
+
+    public float spawnWait;
+    public float startWait;
+    public float wavewait;
+    private bool restart;
+    
+
+    void Start()
+    {
+        PlayerData.data.gameOver = false;
+        restart = false;
+        levelText.text = "Level 1";
+        restartText.text = "";
+        gameOverText.text = "";
+        PlayerData.data.score = 0;
+        UpdateScore();
+
+        waveCount = 1;
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        
+        if(PlayerData.data.score != currentScore)
+        {
+            UpdateScore();
+        }
+        
+        
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Destroy(PlayerData.data);
+                SceneManager.LoadScene("Menu");
+            }
+        }
+
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(wavewait); 
+
+            if (PlayerData.data.gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                gameOverText.text = "Game Over!";
+                restart = true;
+                break;
+            }
+
+            if (waveCount % 2 == 0)
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+            waveCount++;
+        }
+    }
+    
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + PlayerData.data.score;
+        currentScore = PlayerData.data.score;
+    }
+
+   
+
+
+}
